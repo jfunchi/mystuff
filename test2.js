30a104cc337fb6e07ff31739c8e3b048c392a6a6
jfunchi <54717529+jfunchi@users.noreply.github.com>
2020-04-28 17:59:41 -0400

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,39 @@
+jQuery("button[type='submit']").on('load', function() {
+    console.log('iframe loaded?');
+});
+
+jQuery(document).ready(function() {
+    // var x = document.getElementsByTagName("iframe");
+    // var z = jQuery('#form-wrapper iframe');
+    // var y = jQuery('#form-wrapper iframe').html();
+    
+    // console.log(x);
+    // console.log(y);
+    
+
+    // $(x[1]).on('load', function() {
+    //     console.log('loaded from bottom');
+    //     alert("loaded from bottom")
+    // });
+
+
+    /*********************/
+    alert("document is ready jquery");
+    
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    alert("DomContentLoaded");
+});
+
+window.addEventListener('load', function(){
+    alert("Hell yeah");
+    /*jQuery('#text-00000008-acc').load(function() {
+         var gimme = jQuery("#text-00000008-acc")
+         console.log("look: " + gimme);;
+     });*/
+     var test1 = jQuery('#text-00000008-acc');
+     console.log('test1');
+});
+
+
